f00b3e6506c240ea5a7564f4928776f8ed5a7dcb
Vladimir Yashin <vladimir.yashin@mytona.com>
2022-01-21 19:10:09 +0300

    First commit

---
+++ Assets/TestClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using LiteNetLib;
+using LiteNetLib.Utils;
+using UnityEngine;
+
+public class TestClient : MonoBehaviour, INetEventListener
+{
+    private NetManager _netManager;
+    
+    private NetDataWriter _dataWriter;
+    private NetDataReader _dataReader;
+
+    private void Start()
+    {
+        StartClient();
+
+        _dataWriter = new NetDataWriter();
+        _dataReader = new NetDataReader();
+    }
+
+    private void StartClient()
+    {
+        _netManager = new NetManager(this)
+        {
+            UnconnectedMessagesEnabled = true,
+            UpdateTime = 15
+        };
+        _netManager.Start();
+    }
+
+    [ContextMenu(nameof(ConnectLocalhost))]
+    public void ConnectLocalhost()
+    {
+        var ip = IPAddress.Parse("127.0.0.1");
+        int port = 7777;
+        var endPoint = new IPEndPoint(ip, port);
+        Connect(endPoint);
+    }
+
+    public void Connect(IPEndPoint endPoint)
+    {
+        Debug.Log($"{name} | Connecting to {endPoint}");
+        _netManager.Connect(endPoint, "LiteNetGame");
+    }
+
+    public void OnNetworkError(IPEndPoint endPoint, SocketError socketError)
+    {
+        Debug.Log($"{name} | OnNetworkError (socketError: {socketError})");
+    }
+
+    public void OnNetworkReceive(NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
+    {
+    }
+
+    public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+    {
+    }
+    
+    public void OnPeerConnected(NetPeer peer)
+    {
+    }
+
+    public void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+    {
+    }
+
+    public void OnNetworkLatencyUpdate(NetPeer peer, int latency)
+    {
+    }
+
+    public void OnConnectionRequest(ConnectionRequest request)
+    {
+    }
+}
---
+++ Assets/TestServer.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using LiteNetLib;
+using LiteNetLib.Utils;
+using UnityEngine;
+
+public class TestServer : MonoBehaviour, INetEventListener
+{
+    private readonly int defaultPort = 7777;
+    private readonly int defaultMaxPlayers = 1000;
+    
+    private NetManager _netManager;
+    
+    private NetPeer[] _peers;
+    private NetDataReader _dataReader;
+    private NetDataWriter _dataWriter;
+    
+    
+    [ContextMenu(nameof(StartServer))]
+    public void StartServer()
+    {
+        StartServer(defaultPort, defaultMaxPlayers);
+    }
+
+    public void StartServer(int port, int maxPlayers)
+    {
+        _peers = new NetPeer[maxPlayers];
+        _dataReader = new NetDataReader();
+        _dataWriter = new NetDataWriter();
+        
+        _netManager = new NetManager(this)
+        {
+            BroadcastReceiveEnabled = true,
+            UpdateTime = 15
+        };
+        _netManager.Start(port);
+        
+        Debug.Log($"{name} | Started on port: {port}");
+    }
+    
+    private void Update()
+    {
+        _netManager?.PollEvents();
+    }
+    
+    public void OnPeerConnected(NetPeer peer)
+    {
+        Debug.Log($"{name} | OnPeerConnected (IP: {peer.EndPoint})");
+    }
+
+    public void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+    {
+        Debug.Log($"{name} | OnPeerDisconnected (IP: {peer.EndPoint}, {disconnectInfo})");
+    }
+
+    public void OnNetworkError(IPEndPoint endPoint, SocketError socketError)
+    {
+        Debug.Log($"{name} | OnNetworkError (socketError: {socketError})");
+    }
+
+    public void OnNetworkReceive(NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
+    {
+        Debug.Log($"{name} | OnNetworkReceive");
+    }
+
+    public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+    {
+        Debug.Log($"{name} | OnNetworkReceiveUnconnected");
+    }
+
+    public void OnNetworkLatencyUpdate(NetPeer peer, int latency)
+    {
+        //Debug.Log($"{name} | OnNetworkLatencyUpdate {peer.EndPoint} | latency: {latency}");
+    }
+
+    public void OnConnectionRequest(ConnectionRequest request)
+    {
+        Debug.Log($"{name} | OnConnectionRequest from {request.RemoteEndPoint}");
+        var netPeer = request.AcceptIfKey("LiteNetGame");
+    }
+    
+}
