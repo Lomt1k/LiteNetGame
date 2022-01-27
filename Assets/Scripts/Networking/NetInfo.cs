e4dfb169348f4ec04264428f2b51dea5e8a0f98b
Lomt1k <yashin2236@gmail.com>
2022-01-27 23:03:04 +0300

    Main Scene UI (part 3) + NetStarter class

--- Assets/Scripts/Networking/NetInfo.cs
+++ Assets/Scripts/Networking/NetInfo.cs
@@ -1,17 +1,35 @@
-﻿using Networking.Client;
-using Networking.Server;
-
-namespace Networking
+﻿namespace Networking
 {
+    using Client;
+    using Server;
+    
+    public enum NetMode : byte
+    {
+        None,
+        Client,
+        Server,
+        Host
+    }
+    
     public static class NetInfo
     {
         public const string connectionKey = "LiteNetGame";
+        public const int maxPlayersLimit = ushort.MaxValue; //во всех пакетах ID игроков отсылается в ushort (2 байта)
+        public const byte minNicknameLength = 3;
+        public const byte maxNicknameLength = 24;
 
-        public static bool isServer => GameServer.instance != null && GameServer.instance.isRunning;
-        public static bool isClient => GameClient.instance != null && GameClient.instance.isStarted;
-        public static bool isHost => isClient && isServer;
+        public static NetMode mode { get; private set; } = NetMode.None;
 
+        public static bool isServer => mode == NetMode.Server || mode == NetMode.Host;
+        public static bool isClient => mode == NetMode.Client || mode == NetMode.Host;
+        public static bool isHost => mode == NetMode.Host;
+        
         public static ClientPlayer minePlayer => GameClient.instance?.players?.minePlayer;
+
+        public static void SetMode(NetMode _mode)
+        {
+            mode = _mode;
+        }
 
 
     }
---
+++ Assets/Scripts/Networking/NetStarter.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using Networking.Client;
+using Networking.Server;
+using UnityEngine;
+
+namespace Networking
+{
+    public static class NetStarter
+    {
+        public const string localhost = "127.0.0.1";
+        
+        public static void StartServer(int port, int maxPlayers)
+        {
+            if (maxPlayers < 1 || maxPlayers > NetInfo.maxPlayersLimit)
+            {
+                Debug.LogError($"Установлен некорректный лимит игроков (требуется от 1 до {NetInfo.maxPlayersLimit})");
+                return;
+            }
+            if (GameServer.instance == null)
+            {
+                Debug.LogError($"На сцене отсутствует объект с компонентом {nameof(GameServer)}");
+                return;
+            }
+            
+            GameServer.instance.StartServer(port, maxPlayers);
+            NetInfo.SetMode(NetMode.Server);
+        }
+
+        public static void StartClient(string ipString, int port, string nickname)
+        {
+            if (!IPAddress.TryParse(ipString, out var ipAddress))
+            {
+                Debug.LogError("IP адрес введен некорректно");
+                return;
+            }
+            if (nickname.Length > NetInfo.maxNicknameLength || nickname.Length < NetInfo.minNicknameLength)
+            {
+                Debug.LogError($"Некорретный никнейм (длина должна быть " +
+                               $"от {NetInfo.minNicknameLength} до {NetInfo.maxNicknameLength} символов)");
+                return;
+            }
+            if (GameServer.instance == null)
+            {
+                Debug.LogError($"На сцене отсутствует объект с компонентом {nameof(GameClient)}");
+                return;
+            }
+            
+            var endPoint = new IPEndPoint(ipAddress, port);
+            GameClient.instance.Connect(endPoint);
+            NetInfo.SetMode(NetMode.Client);
+        }
+        
+        public static void StartHost(int port, int maxPlayers, string nickname)
+        {
+            StartServer(port, maxPlayers);
+            StartClient(localhost, port, nickname);
+            NetInfo.SetMode(NetMode.Host);
+        }
+        
+        
+    }
+}
