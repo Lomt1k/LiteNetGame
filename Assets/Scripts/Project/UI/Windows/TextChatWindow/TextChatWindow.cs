718918230740ac6dfb172a163d4e2823e2fe6b8c
Lomt1k <yashin2236@gmail.com>
2022-01-28 23:36:13 +0300

    Text Chat (commit #3)

--- Assets/Scripts/Project/UI/Windows/TextChatWindow/TextChatWindow.cs
+++ Assets/Scripts/Project/UI/Windows/TextChatWindow/TextChatWindow.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Project.UI.Windows.TextChatWindow
 {
@@ -13,13 +13,30 @@ namespace Project.UI.Windows.TextChatWindow
         
         [SerializeField] private Transform _textChatContent;
         [SerializeField] private TextMessage _messagePrefab;
+        [SerializeField] private TMP_InputField _inputField;
+        [SerializeField] private CanvasGroup _inputFieldGroup;
 
         private Queue<TextMessage> _messages = new Queue<TextMessage>(maxMessages);
+        private bool _isInputFieldActive;
 
         public override void OnCreated()
         {
             base.OnCreated();
             instance = this;
+            ToggleInputField(false);
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            _inputField.onDeselect.AddListener(OnInputEnd);
+            _inputField.onEndEdit.AddListener(OnInputEnd);
+        }
+        
+        private void UnsubscribeEvents()
+        {
+            _inputField.onDeselect.RemoveListener(OnInputEnd);
+            _inputField.onEndEdit.RemoveListener(OnInputEnd);
         }
 
         public void AddMessage(string text)
@@ -34,8 +51,51 @@ namespace Project.UI.Windows.TextChatWindow
             
             Debug.Log(text);
         }
-        
-        
+
+        private void ToggleInputField(bool state)
+        {
+            _inputFieldGroup.alpha = state ? 1f : 0f;
+            _inputFieldGroup.interactable = state;
+            _inputFieldGroup.blocksRaycasts = state;
+            _isInputFieldActive = state;
+
+            if (!state)
+            {
+                _inputField.text = string.Empty;
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F6) || Input.GetKeyDown(KeyCode.T))
+            {
+                if (!_isInputFieldActive)
+                {
+                    ToggleInputField(true);
+                    _inputField.ActivateInputField();
+                }
+                
+            }
+        }
+
+        private void OnInputEnd(string inputText)
+        {
+            if (!_isInputFieldActive)
+                return;
+
+            bool enterPressed = Input.GetKeyDown(KeyCode.Return);
+            if (enterPressed && inputText.Length > 1)
+            {
+                //TODO send message
+            }
+            
+            ToggleInputField(false);
+        }
+
+        private void OnDestroy()
+        {
+            UnsubscribeEvents();
+        }
     }
 }
 
