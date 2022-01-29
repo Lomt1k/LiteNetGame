614f548c623ba4a0460e2068e50cc0fe221617ee
Lomt1k <yashin2236@gmail.com>
2022-01-29 13:47:20 +0300

    fix Text Chat UI

--- Assets/Scripts/Project/UI/Windows/TextChatWindow/TextChatWindow.cs
+++ Assets/Scripts/Project/UI/Windows/TextChatWindow/TextChatWindow.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 namespace Project.UI.Windows.TextChatWindow
 {
@@ -12,11 +14,14 @@ namespace Project.UI.Windows.TextChatWindow
         public static TextChatWindow instance;
         
         [SerializeField] private Transform _textChatContent;
+        [SerializeField] private Scrollbar _scrollbar;
         [SerializeField] private TextMessage _messagePrefab;
         [SerializeField] private TMP_InputField _inputField;
         [SerializeField] private CanvasGroup _inputFieldGroup;
+        [SerializeField] private CanvasGroup _scrollGroup;
 
         private Queue<TextMessage> _messages = new Queue<TextMessage>(maxMessages);
+        private Coroutine _scrollCoroutine;
         private bool _isInputFieldActive;
 
         public override void OnCreated()
@@ -24,6 +29,7 @@ namespace Project.UI.Windows.TextChatWindow
             base.OnCreated();
             instance = this;
             ToggleInputField(false);
+            ToggleScrollVisibility(false);
             SubscribeEvents();
         }
 
@@ -48,8 +54,8 @@ namespace Project.UI.Windows.TextChatWindow
             message.SetText(text);
             message.transform.SetAsLastSibling();
             _messages.Enqueue(message);
-            
-            Debug.Log(text);
+
+            ScrollToLastMessage();
         }
 
         private void ToggleInputField(bool state)
@@ -84,12 +90,38 @@ namespace Project.UI.Windows.TextChatWindow
                 return;
 
             bool enterPressed = Input.GetKeyDown(KeyCode.Return);
-            if (enterPressed && inputText.Length > 1)
+            if (enterPressed && !string.IsNullOrWhiteSpace(inputText))
             {
                 Networking.Client.Sending.ClientSending_TextChat.SendTextChatMessage(inputText);
             }
             
             ToggleInputField(false);
+        }
+
+        private void ScrollToLastMessage()
+        {
+            if (_scrollCoroutine != null)
+            {
+                StopCoroutine(_scrollCoroutine);
+            }
+            _scrollCoroutine = StartCoroutine(ScrollToLastMessageCoroutine());
+        }
+
+        private IEnumerator ScrollToLastMessageCoroutine()
+        {
+            for (int i = 0; i < 2; i++) //wait 2 frames after adds last message to correct scroll
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            _scrollCoroutine = null;
+            _scrollbar.value = 0;
+        }
+
+        private void ToggleScrollVisibility(bool state)
+        {
+            _scrollGroup.alpha = state ? 1f : 0f;
+            _scrollGroup.interactable = state;
+            _scrollGroup.blocksRaycasts = state;
         }
 
         private void OnDestroy()
