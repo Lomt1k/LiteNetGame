daf92af226f5893e614aa28a456db441dfaf8122
Lomt1k <yashin2236@gmail.com>
2022-02-12 18:13:19 +0300

    ByteHelper: Реализовал методы для запаковки 8 bool в одном байте

---
+++ Assets/Scripts/Utils/ByteHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class ByteHelper
+    {
+        public static byte PackBooleansToByte(params bool[] booleans)
+        {
+            if (booleans.Length > 8)
+                throw new ArgumentException("PackBooleansToByte allows maximum of 8 values");
+            
+            byte result = 0;
+            for (byte i = 0; i < booleans.Length; i++)
+            {
+                if (booleans[i])
+                    result |= (byte)(1 << i);
+            }
+
+            return result;
+        }
+
+        public static bool[] GetBooleansFromByte(byte bitArray)
+        {
+            return new[]
+            {
+                ((bitArray & ( 1 << 0)) != 0),
+                ((bitArray & ( 1 << 1)) != 0),
+                ((bitArray & ( 1 << 2)) != 0),
+                ((bitArray & ( 1 << 3)) != 0),
+                ((bitArray & ( 1 << 4)) != 0),
+                ((bitArray & ( 1 << 5)) != 0),
+                ((bitArray & ( 1 << 6)) != 0),
+                ((bitArray & ( 1 << 7)) != 0),
+            };
+        }
+        
+        
+    }
+}
